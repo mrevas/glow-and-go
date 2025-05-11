caacbe3cd17ccf78bd6504d54a0a1c80e4f3f340
Marc Rivas <76574248+mrevas@users.noreply.github.com>
2025-05-11 10:56:27 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+import Budget from './Budget';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/budget" element={<Budget />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
